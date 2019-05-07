2515b4e6da1062cd8a4df15755df5b483fb35271
lucas <lucasfneves14@gmail.com>
2019-05-07 15:48:36 -0300

    last commit

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -3,10 +3,10 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
       :provider => 'AWS',
-      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ḰEY']
     }
-    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.fog_directory = ENV['AWS_BUCKET']
     config.storage = :fog
   else
     config.storage = :file
