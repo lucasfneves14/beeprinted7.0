93603aa4e689269b19c922a70efed676bff8e9a9
lucas <lucasfneves14@gmail.com>
2019-05-07 11:33:27 -0300

    last commit

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RevisaoBee70
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 	config.i18n.default_locale = 'pt-BR'
+	config.assets.precompile += %w( ckeditor/* )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
