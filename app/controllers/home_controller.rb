4762ac9145ba625eb3c033c7fe61d927cb34acc4
lucas <lucasfneves14@gmail.com>
2019-05-14 11:31:57 -0300

    last commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   layout false
   def index
   	@blog = Blog.includes(:user).all.order('created_at DESC').limit(4)
-    @portfolio = Post.includes(:user, :services).all.order('created_at DESC').limit(8)
+    @portfolio = Post.includes(:user, :services).all.order('created_at DESC').limit(10)
     @contato = Contato.new
   end
 end
