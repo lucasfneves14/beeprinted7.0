cf9ef49f0653baf5d347ae39d58eed6e0b46cb90
lucas <lucasfneves14@gmail.com>
2019-07-23 20:14:15 -0300

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,16 @@ class HomeController < ApplicationController
   def campanha
   	@portfolio = Post.includes(:user, :services).all.order('created_at DESC').limit(10)
     @contato = Contato.new
+    @orcamento = Orcamento.new
+    @arquivo = Arquivo.new
+    @modeling = Modeling.new
+    @reference = Reference.new
+    if current_user
+      @orcamento.name = "#{current_user.name} #{current_user.sobrenome}"
+      @modeling.name = "#{current_user.name} #{current_user.sobrenome}"
+      @orcamento.email = current_user.email
+      @modeling.email = current_user.email
+    end
   end
 
 end
