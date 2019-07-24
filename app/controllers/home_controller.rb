a7b8c86b6b74737645079a4f81f5cf9e85c5d429
lucas <lucasfneves14@gmail.com>
2019-07-24 15:48:48 -0300

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   layout false
-  before_action :authenticate_user!, raise: false
-  skip_before_action :authenticate_user!, only: [:index]
   def index
   	#@blog = Blog.includes(:user).all.order('created_at DESC').limit(4)
     @portfolio = Post.includes(:user, :services).all.order('created_at DESC').limit(9)
