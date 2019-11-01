e34bc031a74a9cdb9a73b866ae7cadfa2b16e718
lucas <lucasfneves14@gmail.com>
2019-10-31 22:28:29 -0300

    first commit

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module RevisaoBee70
 	config.i18n.default_locale = 'pt-BR'
 	config.assets.precompile += %w( ckeditor/* )
   config.exceptions_app = self.routes
+  config.time_zone = 'Brasilia'
+  config.active_record.default_timezone = :local # Or :utc
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
