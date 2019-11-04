0f71091799e4c0730f951c8f60dec54566fab642
lucas <lucasfneves14@gmail.com>
2019-11-04 15:00:46 -0300

    first commit

--- app/controllers/contatos_controller.rb
+++ app/controllers/contatos_controller.rb
@@ -6,7 +6,26 @@ class ContatosController < ApplicationController
 
 	def create
 		@contato = Contato.create(contato_params)
-		if @contato.save
+		@adicionado = Adicionado.create()
+		@adicionado.name = @contato.name
+		@adicionado.email = @contato.email
+		@adicionado.description = @contato.mensagem
+		@adicionado.responsavel = "Iago"
+
+
+		@mes = Date.today.strftime("%m")
+    	@ano = Date.today.strftime("%Y")
+
+    	@orcamentos = Orcamento.where('extract(month  from created_at) = ?', @mes)
+    	@modelagens = Modeling.where('extract(month  from created_at) = ?', @mes)
+    	@adicionados = Adicionado.where('extract(month  from created_at) = ?', @mes)
+    	@planilha = (@modelagens + @orcamentos + @adicionados).sort{|a,b| a.created_at <=> b.created_at }
+
+    	identificador = (@ano.to_i*100 + @mes.to_i)*1000 + @planilha.count + 1
+
+    	@orcamento.identificador = identificador
+
+		if @contato.save && @adicionado.save
 			ContatoMailer.contato_email(@contato).deliver
 			flash[:success] = "Seu pedido de contato foi enviado! Em breve, entraremos em contato por email."
 			redirect_to contato_sucesso_path
