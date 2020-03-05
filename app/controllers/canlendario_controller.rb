556d9d480bd5f94e4a44cd57b423275b8b169347
lucas <lucasfneves14@gmail.com>
2020-03-05 15:21:30 -0300

    first commit

--- app/controllers/canlendario_controller.rb
+++ app/controllers/canlendario_controller.rb
@@ -3,9 +3,9 @@ class CanlendarioController < ApplicationController
 	before_action :admin
 	layout 'system/navbar'
 	def show
-		@orcamentos = Orcamento.where('extract(month  from created_at) = ?', 3).where('extract(YEAR  from created_at) = ?', 2020).where(status: "Fechado")
-		@modelagens = Modeling.where('extract(month  from created_at) = ?', 3).where('extract(YEAR  from created_at) = ?', 2020).where(status: "Fechado")
-		@adicionados = Adicionado.where('extract(month  from created_at) = ?', 3).where('extract(YEAR  from created_at) = ?', 2020).where(status: "Fechado")
+		@orcamentos = Orcamento.where(status: "Fechado")
+		@modelagens = Modeling.where(status: "Fechado")
+		@adicionados = Adicionado.where(status: "Fechado")
 
 		@planilha = (@modelagens + @orcamentos + @adicionados).sort{|a,b| a.identificador <=> b.identificador }.reverse
 	end
