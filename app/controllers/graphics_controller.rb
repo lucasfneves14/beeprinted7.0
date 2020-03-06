2c43063592ae0d6e47f641aa166caa2263c00594
lucas <lucasfneves14@gmail.com>
2020-03-06 13:56:07 -0300

    first commit

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -8,6 +8,12 @@ class GraphicsController < ApplicationController
 	@adicionados = Adicionado.where.not(status: "Cancelado")
 
 	@planilha = (@modelagens + @orcamentos + @adicionados)
+	@iago = @planilha.select{|orcamento| orcamento.responsavel == "Iago"}
+	@iago = @iago.group_by_week {|u| u.created_at}.map { |k, v| [k, v.count] }.to_h
+	@thierre = @planilha.select{|orcamento| orcamento.responsavel == "Thierre"}
+	@thierre = @thierre.group_by_week {|u| u.created_at}.map { |k, v| [k, v.count] }.to_h
+
+
 	@planilha = @planilha.group_by_week {|u| u.created_at}.map { |k, v| [k, v.count] }.to_h
   end
 
